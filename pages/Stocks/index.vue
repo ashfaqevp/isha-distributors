4d8ec4fc0f58a506d8ddb35d760930c40b09319f
ashfaqevp <ashfaqevp@gmail.com>
2023-10-26 01:34:54 +0530

    initilized tabs in stocks

---
+++ pages/Stocks/index.vue
@@ -0,0 +1,85 @@
+<script setup>
+import { collection, getDocs } from 'firebase/firestore'
+
+const { db } = useFirebaseStore()
+const router = useRouter()
+
+const loading = ref(false)
+
+async function fetchNewStock() {
+//   loading.value = true
+//   try {
+//     const querySnapshot = await getDocs(collection(db, 'products'))
+//     productList.value = querySnapshot.docs.map(doc => ({
+//       id: doc.id,
+//       ...doc.data(),
+//     }))
+//   }
+//   catch (error) {
+//     console.error('Error fetching data:', error)
+//   }
+
+//   finally {
+//     loading.value = false
+//   }
+}
+
+onMounted (async () => {
+  await fetchNewStock()
+  // other functions
+})
+
+const tab = ref(null)
+const allTabs = ['Current', 'New', 'Damaged']
+
+function openAdd() {
+  console.log('add')
+}
+</script>
+
+<template>
+  <v-app-bar
+    class="px-0"
+    color="primary"
+  >
+    <template #prepend>
+      <v-app-bar-nav-icon class="mx-0">
+        <Icon name="eva:arrow-back-outline" size="24" @click="router.back()" />
+      </v-app-bar-nav-icon>
+    </template>
+
+    <div class="flex justify-left w-full">
+      <v-app-bar-title>
+        Stocks
+      </v-app-bar-title>
+    </div>
+
+    <template #append>
+      <v-app-bar-nav-icon @click="openAdd">
+        <Icon name="gg:add" size="24" />
+      </v-app-bar-nav-icon>
+    </template>
+
+    <template #extension>
+      <div class="w-full px-0">
+        <v-tabs
+          v-model="tab"
+          align-tabs="title"
+        >
+          <v-tab
+            v-for="item in allTabs"
+            :key="item"
+            :value="item"
+          >
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+      </div>
+    </template>
+  </v-app-bar>
+  <!-- <div>
+    jhew
+  </div>
+
+  {{ 'tab' }} -->
+</template>
