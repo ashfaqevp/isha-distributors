8e74fcecc3e61378dd5cc9c435914570e85f95ca
ashfaqevp <ashfaqevp@gmail.com>
2023-12-03 02:08:52 +0530

    shops page completed bottom nav testing

---
+++ components/Shops/Dialog/Add.vue
@@ -0,0 +1,217 @@
+import { Body } from '#build/components';
+<script setup>
+import { addDoc, collection, doc, getDoc } from 'firebase/firestore'
+
+const props = defineProps({
+  modelValue: Boolean,
+})
+
+const emit = defineEmits(['update:modelValue', 'refresh'])
+const { db } = useFirebaseStore()
+
+const { shopCategories } = useUtils()
+const { setToast } = useMainStore()
+
+const placeList = ref([])
+
+const body = ref({})
+const form = ref()
+const isOpen = ref(false)
+const loading = ref(false)
+
+const toast = ref({ value: false, message: '', status: '' })
+
+watch(() => props?.modelValue?.length, () => {
+  if (props?.modelValue) {
+    isOpen.value = true
+    body.value.type = 'normal'
+    body.value.credit_limit = 1000
+    body.value.pending = 0
+  }
+}, { deep: true, immediate: true })
+
+function onCancel() {
+  form.value.reset()
+  form.value.resetValidation()
+  emit('update:modelValue', false)
+  isOpen.value = false
+}
+
+async function addShop() {
+  const { valid } = await form.value.validate()
+
+  if (!valid) {
+    setToast(true, 'Complete all required fields', 'error')
+    return
+  }
+
+  loading.value = true
+
+  try {
+    const shop = body.value
+    const dbRef = collection(db, 'shops')
+    await addDoc(dbRef, shop)
+    setToast(true, 'Shop added to shop successfully', 'success')
+    emit('refresh')
+    onCancel()
+  }
+  catch (e) {
+    setToast(true, 'Shop not saved', 'error')
+    console.error(e)
+  }
+  finally {
+    loading.value = false
+  }
+}
+
+async function fetchPlaces() {
+  try {
+    const docRef = doc(db, 'others', 'places')
+    const docSnapshot = await getDoc(docRef)
+    if (docSnapshot.exists())
+      placeList.value = docSnapshot.data().place_list || []
+  }
+  catch (error) {
+    console.error('Error fetching Places:', error)
+  }
+}
+
+onMounted (async () => {
+  await fetchPlaces()
+})
+</script>
+
+<template>
+  <v-dialog
+    v-model="isOpen"
+    persistent
+    width="420"
+  >
+    <v-card class="w-full">
+      <v-card-title>
+        <div class=" flex gap-x-3 mt-3 ">
+          <button>
+            <Icon name="eva:arrow-back-outline" size="24" @click="onCancel" />
+          </button>
+          Add New Shop
+        </div>
+      </v-card-title>
+
+      <v-divider class="my-2" />
+      <v-card-text>
+        <v-form ref="form" class="mt-1">
+          <v-text-field
+            v-model="body.name"
+            :rules="[v => !!v || '']"
+            label="Shop Name"
+            required
+            variant="outlined"
+          />
+
+          <v-select
+            v-model="body.place"
+            :items="placeList"
+            :rules="[v => !!v || '']"
+            label="Place"
+            required
+            variant="outlined"
+          />
+
+          <v-text-field
+            v-model="body.phone"
+            :rules="[v => !!v || '', v => (v && v.length === 10) || '']"
+            label="Phone"
+            required
+            type="number"
+            prefix="+91"
+            variant="outlined"
+          />
+
+          <v-row dense>
+            <v-col
+              cols="6"
+              sm="6"
+            >
+              <v-select
+                v-model="body.type"
+                :items="shopCategories"
+                :rules="[v => !!v || '']"
+                label="Type"
+                required
+                variant="outlined"
+              />
+            </v-col>
+            <v-col
+              cols="6"
+              sm="6"
+            >
+              <v-text-field
+                v-model="body.credit_limit"
+                label="Credit Limit"
+                required
+                prefix="₹"
+                type="number"
+                variant="outlined"
+              />
+            </v-col>
+          </v-row>
+
+          <v-text-field
+            v-model="body.pending"
+            label="Pending"
+            required
+            prefix="₹"
+            type="number"
+
+            variant="outlined"
+          />
+
+          <v-footer class="px-0 pt-0">
+            <v-row dense>
+              <v-col
+                cols="6"
+                sm="6"
+              >
+                <v-btn
+                  color="gray"
+                  variant="outlined"
+                  block
+                  height="44"
+                  :disabled="loading"
+                  @click="onCancel"
+                >
+                  Cancel
+                </v-btn>
+              </v-col>
+              <v-col
+                cols="6"
+                sm="6"
+              >
+                <v-btn
+                  color="primary"
+                  block
+                  height="44"
+                  :loading="loading"
+                  @click="addShop()"
+                >
+                  Save
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-footer>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+
+  <v-snackbar
+    v-model="toast.value"
+    class="z-100 opacity-90"
+    :timeout="2000"
+    :color=" toast?.status === 'success' ? 'success' : 'error'"
+    absolute
+    top
+  >
+    <p>{{ toast?.message }}</p>
+  </v-snackbar>
+</template>
